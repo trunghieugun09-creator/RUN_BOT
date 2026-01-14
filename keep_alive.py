8a4e43322927bae15c354c1e3718e20a9cf422f5
nuxz <trunghieugun09@gmail.com>
2026-01-14 16:15:05 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,12 +5,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "BOT ĐANG HOẠT ĐỘNG — TGHIEUX!"
 
 def run():
-  # Chú ý: port phải trùng với port Wispbyte cấp cho bạn
-  app.run(host='0.0.0.0', port=11113)
-
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
+    
 def keep_alive():
     t = Thread(target=run)
     t.start()
